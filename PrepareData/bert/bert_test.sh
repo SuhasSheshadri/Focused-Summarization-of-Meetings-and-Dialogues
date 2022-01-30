a3d2eb2d6395f8538409578185980f66ca20910e
Suhas Sheshadri <suhas.sheshadri@gmail.com>
2022-01-30 16:40:05 +0100

    Add files via upload

---
+++ PrepareData/bert/bert_test.sh
@@ -0,0 +1,1 @@
+python run_classifier.py --task_name=cola --do_predict=true --data_dir=./data --vocab_file=./cased_L-12_H-768_A-12/vocab.txt --bert_config_file=./cased_L-12_H-768_A-12/bert_config.json --init_checkpoint=./bert_output/model.ckpt-(max number from bert output model) --max_seq_length=128 --output_dir=./bert_output/ --do_lower_case=false
